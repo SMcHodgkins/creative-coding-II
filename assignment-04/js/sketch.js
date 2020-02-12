d7e4e54c1d2a8336db6fc2ec40ddefd490b9e7b0
Scott McHodgkins <scott.mchodgkins@umontana.edu>
2020-02-12 06:52:51 -0700

    hw4

--- assignment-04/js/sketch.js
+++ assignment-04/js/sketch.js
@@ -1,22 +1,6 @@
-var idleFrames = [];
-var idle;
-var i = 0;
-var x = 0;
-let result = [];
-function addImages(data){
-  result = data;
-  var data= "";
 
-  for (var i = 0; i < result.length; i++) {
-    idle = loadImage(result[i]);
-    console.log('assets/'+ result[i])
-    idleFrames[i] = idle;
-  }
-}
 function preload(){
-  //idle = loadImage('images/assets/idle (1).png')
-  //idleFrames[0] = idle;
-  loadStrings('assets/idle.txt', addImages)
+  loadStrings('assets/idle.txt')
 }
 function setup() {
   createCanvas(800, 800);
@@ -24,8 +8,4 @@ function setup() {
 
 function draw() {
   background(0,0,0);
-  image(idleFrames[i],0,0);
 }
-/*class player{
-  constructor()
-}*/
